47f6b1adce300d07a6438d144d6b9b1ef1d18daf
Naruto <umairapi1995@gmail.com>
2020-11-15 19:35:07 +0500

    command line argument equality code

---
+++ cmdline/Main.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String[] args){
+        if(args[0].equals(args[1])){
+            System.out.println("Same Strings");
+        }
+        else{
+            System.out.println("Different Strings");
+        }
+    }
+}
